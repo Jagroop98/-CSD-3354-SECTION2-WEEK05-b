1f95c88a789d419c27976f71f178091ecec6425c
visualstudio <visualstudio@122-PC26>
2019-02-06 12:03:53 -0500

    ss

--- section 2 new/Program.cs
+++ section 2 new/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace section_2_new
 {
-    class Program
+
+    class singh
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
+            Console.ReadLine();
+
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+
+                Console.WriteLine("i is {0}", i);
+
+                if (i > 10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
+
+
+
